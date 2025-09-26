d149fb0cc40f40a317569c0f162ff3470332ba8d
ella-walker <wella2@byu.edu>
2025-09-26 12:30:36 -0600

    testing Book Class

--- digital_library.py
+++ digital_library.py
@@ -4,7 +4,20 @@ import pandas as pd
 
 ## Book Class
 class Book:
-    pass
+    def get_age(book_id, title, author, pages, published_year, genre):
+        current_year = 2025
+        age = current_year - published_year
+        return age
+
+    def is_long(book_id, title, author, pages, published_year, genre):
+        if pages > 300:
+            return True
+        else:
+            return False
+
+    def summary(book_id, title, author, pages, published_year, genre):
+        return (f"Book ID {book_id}: {title} by {author} has {pages} pages. It was first published in {published_year} "
+                f"and belongs to the {genre} genre.")
     
 class Library:
     pass
